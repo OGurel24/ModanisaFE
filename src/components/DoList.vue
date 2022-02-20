3bc301b6afbaecda53e36a0c06d71bffc6fd299c
OGurel24 <onur.gurel@useinsider.com>
2022-02-20 14:21:27 +0300

    Do List structure

---
+++ src/components/DoList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <label for="input">New Item</label>
+    <input type="text" id="input" v-model="item">
+    <input type="submit" id="submit" @click="addItem">
+    <br><br>
+    <ol>
+      <li v-for="item in items" :key="item">{{ item }}</li>
+    </ol>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "DoList",
+  data() {
+    return {
+      item: "",
+      items: []
+    }
+  },
+  methods: {
+    addItem() {
+      this.items.push(this.item)
+      this.item = ""
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
